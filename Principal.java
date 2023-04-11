3ab23569c4416460d8ba049f909b86a7006b0fe8
LeoValleWeb <leomartinsvalle@gmail.com>
2023-04-11 15:03:18 +0000

    save1

---
+++ Principal.java
@@ -0,0 +1,20 @@
+public class Principal {
+    
+    public static void main(String[] args){
+
+        ContaBancaria conta = new ContaBancaria();
+        conta.setNumeroConta(2230);
+        conta.setTitular("Leonardo Valle");
+        conta.setSaldo(100);
+
+        System.out.println("Saldo atual: "+conta.getSaldo());
+
+        conta.depositaValor(500);
+
+        System.out.println("Saldo atual: "+conta.getSaldo());
+
+        conta.sacaValor(700);
+        System.out.println("Saldo atual: "+conta.getSaldo());
+
+    }
+}
